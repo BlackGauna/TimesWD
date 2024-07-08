73a189ea07c44093d594b31d6e5b6c6a7331780a
Onur Hokkaömeroglu <onur.hok@gmx.net>
2024-07-08 19:53:16 +0200

    added timer page with buttons

---
+++ frontend/src/components/Timer.tsx
@@ -0,0 +1,43 @@
+import Button from "react-bootstrap/Button"
+import svgStyles from "@styles/svg.module.css"
+import { Container } from "react-bootstrap"
+function Timer() {
+  return (
+    <Container className="vw-100 vh-100 d-flex align-items-center justify-content-center">
+      <div className="w-100 d-flex justify-content-around">
+        <Button
+          className={`rounded-circle ${svgStyles.buttplay}`}
+          variant="outline-primary"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            className={`${svgStyles.svg} ${svgStyles.play}`}
+            //   fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
+          </svg>
+        </Button>
+        <Button
+          className={`rounded-circle ${svgStyles.buttstop}`}
+          variant="outline-primary"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className={`${svgStyles.svg} ${svgStyles.stop}`}
+            viewBox="0 0 16 16"
+          >
+            <path d="M5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5A1.5 1.5 0 0 1 5 3.5" />
+          </svg>
+        </Button>
+      </div>
+    </Container>
+  )
+}
+
+export default Timer
